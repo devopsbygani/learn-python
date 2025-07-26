2383c2a034c50b750a94d07040c27b888d21b37a
ganeshmoka <ganeshmoka1999@gmail.com>
2025-07-26 11:45:07 +0530

    list

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+l=[1,2,0,65,8,3]
+l.sort()
+print(l)
+
+#append
+l.append("hey")
+print (l)
+
+#extend
+l.extend([1,2,3,4,"hey"])
+print(l)
+
--- hello.py
+++ hello.py
@@ -1,2 +1,34 @@
 a= 10+10
 print(a)
+
+b= "ganesh"
+print(b,type(b))
+#print(dir(b))
+print(b.upper())
+print(b.index('h'))
+
+#  'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill'
+
+p= True
+q= True
+
+print (p and q)
+print (p or q)
+
+x= 4
+y= 2
+
+print ("addition:",x+y)
+print ("sub:",x-y)
+print ("multi:",x*y)
+print ("division:",x/y)
+print ("modulus:",x%y)  # returns reminder.
+print ("floor division:",x//y) 
+print ("exponential:",x**y)
+
+# list
+l= [1,2,3,"gani",True,[30,40,"moka",False]]
+print(l)
+print(l[3])
+l1=l[5]
+print(l1[0])
